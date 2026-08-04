a4a4ee29cbd5a50dc7ace96a32523980d8079a8d
linsaftw <linsaftw@users.noreply.github.com>
2026-08-04 18:20:49 -0300

    2.0.3

--- src/main/java/dev/_2lstudios/chatsentinel/velocity/utils/Constants.java
+++ src/main/java/dev/_2lstudios/chatsentinel/velocity/utils/Constants.java
@@ -3,7 +3,7 @@ package dev._2lstudios.chatsentinel.velocity.utils;
 public class Constants {
     public static final String ID = "chatsentinel";
     public static final String NAME = "ChatSentinel";
-    public static final String VERSION = "2.0.2";
+    public static final String VERSION = "2.0.3";
     public static final String DESCRIPTION = "Advanced chat management plugin";
     public static final String URL = "https://builtbybit.com/resources/23698/";
     public static final String AUTHOR = "2LS";
